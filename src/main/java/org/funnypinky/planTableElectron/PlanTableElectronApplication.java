b0c07f917953e8c68050cf55cae70e6035070146
Funnypinky <steffenhasler@googlemail.com>
2024-01-09 14:44:02 +0100

    init Repo

--- src/main/java/org/funnypinky/planTableElectron/PlanTableElectronApplication.java
+++ src/main/java/org/funnypinky/planTableElectron/PlanTableElectronApplication.java
@@ -1,6 +1,5 @@
 package org.funnypinky.planTableElectron;
 
-import jakarta.annotation.PostConstruct;
 import org.funnypinky.planTableElectron.Service.EntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlanTableElectronApplication {
-
-	@Autowired
-	private EntityService entityService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanTableElectronApplication.class, args);
